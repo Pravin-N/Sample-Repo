53abc653339dcb06d6789becb32d057fb5ecfea9
Pravin N <Pravin86n@gmail.com>
2020-05-06 08:15:44 -0400

    Added an addition function. changed the get link to google.com

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 import requests
 
-r = requests.get("https://coreyms.com")
+r = requests.get("https://google.com")
 print(r.status_code)
 print(r.ok)
+
+
+def addition(x, y):
+    return int(x) + int(y)
+
+
+print(addition(2, 5))
